cc7475b2723099b918e4b28ca489a8e4de3bc6ae
JuanJose <juanjoseevv@gmail.com>
2025-02-28 00:08:27 -0500

    Agrega configuración de PostCSS, Tailwind CSS y componentes UI, y mejora la estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 export const App = () => {
   return (
     <div>
-      <h1>BUILD MART</h1>
+      <h1 className="text-2xl text-center">BUILD MART</h1>
+      <Button variant="destructive" className="m-5">
+        Hola
+      </Button>
     </div>
   );
 };
