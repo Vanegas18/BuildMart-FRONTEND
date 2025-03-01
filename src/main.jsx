0b6898ef0379c520398e4447db8b567161d80192
JuanJose <juanjoseevv@gmail.com>
2025-02-28 21:20:03 -0500

    Elimina archivos de configuración obsoletos y agrega nuevos archivos de configuración para mejorar la estructura del proyecto y la integración de Tailwind CSS.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
+import "tailwindcss/tailwind.css";
 import { BuildMart } from "./BuildMart";
 
 createRoot(document.getElementById("root")).render(
