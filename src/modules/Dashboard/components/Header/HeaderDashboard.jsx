29f08ac40045eef0e18e49c28d44b74d00ebe4fd
JuanJose <juanjoseevv@gmail.com>
2025-04-30 00:52:52 -0500

    Sin btn de notificacion

--- src/modules/Dashboard/components/Header/HeaderDashboard.jsx
+++ src/modules/Dashboard/components/Header/HeaderDashboard.jsx
@@ -13,15 +13,6 @@ export const HeaderDashboard = () => {
           <div className={styles.searchContainer}></div>
           {/* Contenedor para acciones/botones */}
           <div className={styles.actionContainer}>
-            {/* Botón de notificaciones con badge indicando cantidad */}
-            <Button
-              variant="outline"
-              size="icon"
-              className={styles.notificationButton}>
-              <Bell className={styles.notificationIcon} />
-              {/* Badge con contador de notificaciones */}
-              <span className={styles.notificationBadge}>3</span>
-            </Button>
             {/* Botón para navegar a la tienda */}
             <Link to="/">
               <Button variant="ghost">Ver tienda</Button>
