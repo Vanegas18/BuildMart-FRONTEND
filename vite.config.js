bcb140491c4f66c4a607347dfb3efab2ba9283c6
JuanJose <juanjoseevv@gmail.com>
2025-06-02 10:52:03 -0500

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // ← Cambio aquí
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
